18bd7e8d80619a2e1785c90d84c5a1e74e78c887
Kari Fallos <k.lindseth@hotmail.com>
2020-10-21 18:53:14 -0500

    Added GetBooksfromDatabase Function

---
+++ ReadingRainbow/api-functions/GetBooksFromDatabase.js
@@ -0,0 +1,44 @@
+
+export async function GetBooksFromDatabase() {
+
+    const fullurl = 'http://localhost:5000/api/book/FindfromNeo';
+
+    // create full request URL by joining parts of the string with the base request
+    try{
+
+        const response = await fetch(fullurl,           
+            {
+                mode: 'cors',
+                headers: {
+                  'Access-Control-Allow-Origin':'*'
+                }
+            });
+        const json = await response.json();
+        return json.map((name, index) => ReturnBook(name, index));
+    } catch (err) {
+        console.error(err);
+    } finally {
+        console.log('All tasks complete'); //Feel free to change this
+    }
+ }
+
+ function ReturnBook(name, index)
+ {
+  var book = {
+    Index : index,
+    //BookInformationLink: name.selfLink,
+    Title : name.title,
+    //Authors : name.volumeInfo.authors,
+    Thumbnail : name.thumbnail, 
+    SmallThumbnail : name.smallThumbnail, 
+    PublishDate : name.publishDate,
+    NumberPages : name.numberPages,
+    Description : name.description,
+    ISBN_10 : name.isbN_10, 
+    ISBN_13: name.isbN_13,
+    ISBN_Other: name.isbN_Other,
+    // Categories: name.volumeInfo.categories
+    }
+
+    return book;
+ }
