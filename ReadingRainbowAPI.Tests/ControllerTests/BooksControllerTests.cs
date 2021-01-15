464411ad0c0418cc07d88896a5f285d67a776050
Kari Fallos <k.lindseth@hotmail.com>
2021-01-15 17:38:35 -0600

    Added endpoint and tests to add book to user library

--- ReadingRainbowAPI.Tests/ControllerTests/BooksControllerTests.cs
+++ ReadingRainbowAPI.Tests/ControllerTests/BooksControllerTests.cs
@@ -6,6 +6,7 @@ using ReadingRainbowAPI.Models;
 using ReadingRainbowAPI.Relationships;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using AutoMapper;
@@ -38,19 +39,39 @@ namespace ReadingRainbowAPI.ControllerTests
 
         }
 
-        private Book CreateBook()
+        private void SetupMockBookRepo(Book returnedBook)
+        {
+            _bookRepository 
+                    .Setup(x => x.Single(It.IsAny<Expression<Func<Book, bool>>>()))
+                    .ReturnsAsync(returnedBook);
+            _bookRepository
+                    .Setup(a=>a.Add(It.IsAny<Book>()))
+                    .ReturnsAsync(true);
+            _bookRepository
+                  .Setup(x => x.Update(It.IsAny<Expression<Func<Book, bool>>>(), It.IsAny<Book>()))
+                  .ReturnsAsync(true);
+        }
+
+        private Book CreateBook(List<Genre> genres = null)
         {
             var random = new Random();
             var bookIdExt = random.Next();
 
             // Arrange
-            return new Book(){
+            var book = new Book(){
                 Id = $"xbn56r{bookIdExt}",
                 Title =$"Test Book Title {bookIdExt}",
                 PublishDate  = DateTime.Now.ToShortDateString(),
                 NumberPages  = $"{bookIdExt}",
                 Description  = "Test Book Description",
             };
+
+            if (genres != null)
+            {
+                book.Genres = genres;
+            }
+
+            return book;
 
         }
    
@@ -65,6 +86,19 @@ namespace ReadingRainbowAPI.ControllerTests
                 Profile =$"This is new person number {personId}",
                 Portrait = "Https://PortaitLink",
                 HashedPassword = $"{personId}"
+
+            };
+        }
+
+        private Genre CreateGenre()
+        {
+            var random = new Random();
+            var genreId = random.Next();
+
+            // Arrange
+            return new Genre(){
+                Name = $"newGenre{genreId}",
+                Description =$"This is new genre number {genreId}"
 
             };
         }
@@ -109,15 +143,7 @@ namespace ReadingRainbowAPI.ControllerTests
             // Arrange
             var newBook = CreateBook();
                 
-            _bookRepository 
-                    .Setup(x => x.Single(It.IsAny<Expression<Func<Book, bool>>>()))
-                    .ReturnsAsync(newBook);
-            _bookRepository
-                    .Setup(a=>a.Add(It.IsAny<Book>()))
-                    .ReturnsAsync(true);
-            _bookRepository
-                  .Setup(x => x.Update(It.IsAny<Expression<Func<Book, bool>>>(), It.IsAny<Book>()))
-                  .ReturnsAsync(true);
+            SetupMockBookRepo(newBook);
 
             var bookController = new BookController(_bookRepository.Object, _genreRepository.Object, _mapper);
 
@@ -144,7 +170,6 @@ namespace ReadingRainbowAPI.ControllerTests
                 .Setup(x => x.Single(It.IsAny<Expression<Func<Book, bool>>>()))
                 .ReturnsAsync(newBook);
 
-                // this.GetAllRelated<Genre, InGenre>(b=>b.Id == book.Id, new Genre(),  inGenre);
             _bookRepository
                 .Setup(x => 
                 x.GetAllRelated<Genre, InGenre>(It.IsAny<Expression<Func<Book, bool>>>(), It.IsAny<Genre>(), It.IsAny<InGenre>()))
@@ -193,5 +218,176 @@ namespace ReadingRainbowAPI.ControllerTests
             Assert.Equal(200, okResult.StatusCode);
         }
 
+        [Fact]
+        public async void AddNewBooktoLibraryRoute_Test()
+        {   
+            // Arrange
+            var genre1 = CreateGenre();
+            var newBook = CreateBook(new List<Genre>() {
+                genre1
+            });
+            var person = CreatePerson();
+
+            var library = new List<Book>();
+            var bookGenres = new List<Genre>();
+
+
+            _bookRepository
+            .Setup(x => 
+                x.Relate<Person, InLibrary>(It.IsAny<Expression<Func<Book, bool>>>(), It.IsAny<Expression<Func<Person, bool>>>(), It.IsAny<InLibrary>()))
+                .ReturnsAsync(true)
+                .Callback<Expression<Func<Book, bool>>, Expression<Func<Person, bool>>, InLibrary>(
+                    (exp1, exp2, inLibrary) => { library.Add(newBook); }
+                );
+
+            _genreRepository 
+            .Setup(x => 
+                x.GetRelated<Book, InGenre>(It.IsAny<Expression<Func<Genre, bool>>>(), It.IsAny<Expression<Func<Book, bool>>>(), It.IsAny<InGenre>()))
+                .ReturnsAsync(new List<Book>());
+
+            _genreRepository
+            .Setup(x => 
+                x.Relate<Book, InGenre>(It.IsAny<Expression<Func<Genre, bool>>>(), It.IsAny<Expression<Func<Book, bool>>>(), It.IsAny<InGenre>()))
+                .ReturnsAsync(true)
+                .Callback<Expression<Func<Genre, bool>>, Expression<Func<Book, bool>>, InGenre>(
+                    (exp1, exp2, inLibrary) => { bookGenres.Add(genre1); }
+                );
+         
+            SetupMockBookRepo(CreateBook());
+
+            var bookController = new BookController(_bookRepository.Object, _genreRepository.Object, _mapper);
+
+            // Act
+            var result = await bookController.AddBookToLibrary(person.Name, newBook);
+            var okResult = result as OkResult;
+
+            // Assert
+            Assert.True(okResult != null);
+            Assert.Equal(200, okResult.StatusCode);
+
+            // Verify Correct functions were called / correct callbacks were performed
+            Assert.True(library.Count == 1);
+            Assert.True(library.Where(b=>b.Title == newBook.Title).ToList().Count == 1);
+
+            Assert.True(bookGenres.Count == 1);
+            Assert.True(bookGenres.Where(b=>b.Description == genre1.Description).ToList().Count == 1);
+            
+        }
+
+        [Fact]
+        public async void AddExistingBooktoLibraryRoute_Test()
+        {         
+            // Arrange
+            var genre1 = CreateGenre();
+            var newBook = CreateBook(new List<Genre>() {
+                genre1
+            });
+            var person = CreatePerson();
+
+            var library = new List<Book>();
+
+            _bookRepository
+            .Setup(x => 
+                x.Relate<Person, InLibrary>(It.IsAny<Expression<Func<Book, bool>>>(), It.IsAny<Expression<Func<Person, bool>>>(), It.IsAny<InLibrary>()))
+                .ReturnsAsync(true)
+                .Callback<Expression<Func<Book, bool>>, Expression<Func<Person, bool>>, InLibrary>(
+                    (exp1, exp2, inLibrary) => { library.Add(newBook); }
+                );
+
+            _genreRepository
+            .Setup(x => 
+                x.GetRelated<Book, InGenre>(It.IsAny<Expression<Func<Genre, bool>>>(), It.IsAny<Expression<Func<Book, bool>>>(), It.IsAny<InGenre>()))
+                .ReturnsAsync(new List<Book>() { newBook  });
+         
+            SetupMockBookRepo(newBook);
+
+            var bookController = new BookController(_bookRepository.Object, _genreRepository.Object, _mapper);
+
+            // Act
+            var result = await bookController.AddBookToLibrary(person.Name, newBook);
+            var okResult = result as OkResult;
+
+            // Assert
+            Assert.True(okResult != null);
+            Assert.Equal(200, okResult.StatusCode);
+
+            // Verify Correct functions were called / correct callbacks were performed
+            Assert.True(library.Count == 1);
+            Assert.True(library.Where(b=>b.Title == newBook.Title).ToList().Count == 1);
+            
+        }
+
+        [Fact]
+        public async void AddBooktoLibraryAlreadyInLibraryRoute_Test()
+        {  
+                        // Arrange
+            var genre1 = CreateGenre();
+            var newBook = CreateBook(new List<Genre>() {
+                genre1
+            });
+            var person = CreatePerson();
+
+            var library = new List<Book>();
+            var bookGenres = new List<Genre>();
+
+
+            _bookRepository
+            .Setup(x => 
+                x.Relate<Person, InLibrary>(
+                    It.IsAny<Expression<Func<Book, bool>>>(), 
+                    It.IsAny<Expression<Func<Person, bool>>>(), 
+                    It.IsAny<InLibrary>()))
+                .ReturnsAsync(true)
+                .Callback<Expression<Func<Book, bool>>, Expression<Func<Person, bool>>, InLibrary>(
+                    (exp1, exp2, inLibrary) => { library.Add(newBook); }
+                );
+
+            _bookRepository
+            .Setup(x =>
+                x.GetRelated<Person, InLibrary>(
+                    It.IsAny<Expression<Func<Book,bool>>>(),
+                    It.IsAny<Expression<Func<Person, bool>>>(),
+                    It.IsAny<InLibrary>())
+                )
+                .ReturnsAsync(new List<Person>() {person});
+
+            _genreRepository
+            .Setup(x => 
+                x.GetRelated<Book, InGenre>(
+                    It.IsAny<Expression<Func<Genre, bool>>>(), 
+                    It.IsAny<Expression<Func<Book, bool>>>(), 
+                    It.IsAny<InGenre>()))
+                .ReturnsAsync(new List<Book>() { newBook  });
+
+            _genreRepository
+            .Setup(x => 
+                x.Relate<Book, InGenre>(
+                    It.IsAny<Expression<Func<Genre, bool>>>(), 
+                    It.IsAny<Expression<Func<Book, bool>>>(), 
+                    It.IsAny<InGenre>()))
+                .ReturnsAsync(true)
+                .Callback<Expression<Func<Genre, bool>>, Expression<Func<Book, bool>>, InGenre>(
+                    (exp1, exp2, inLibrary) => { bookGenres.Add(genre1); }
+                );
+         
+            SetupMockBookRepo(newBook);
+
+            var bookController = new BookController(_bookRepository.Object, _genreRepository.Object, _mapper);
+
+            // Act
+            var result = await bookController.AddBookToLibrary(person.Name, newBook);
+            var okResult = result as OkResult;
+
+            // Assert
+            Assert.True(okResult != null);
+            Assert.Equal(200, okResult.StatusCode);
+
+            // Verify Correct functions were called / correct callbacks were performed
+            Assert.True(library.Count == 0);
+            Assert.True(library.Where(b=>b.Title == newBook.Title).ToList().Count == 0);
+
+            // Since Book already had genres associated more should not have been added
+            Assert.True(bookGenres.Count == 0);
+        }
     }
 }
