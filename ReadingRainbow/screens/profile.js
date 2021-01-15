0629cc552a546a90c8ef8021b5e4ffd0a9e50b3c
mbb83 <mbb83@drexel.edu>
2021-01-14 22:11:21 -0500

    Continued trying to get profile results to display on profile screen

--- ReadingRainbow/screens/profile.js
+++ ReadingRainbow/screens/profile.js
@@ -1,38 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, ScrollView } from 'react-native';
+import { View, Text, Button, ScrollView, ActivityIndicator } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { GetUserProfile } from '../api-functions/getUserProfile';
 import { GetUserLibrary } from '../api-functions/getUserLibrary';
 import * as SecureStore from 'expo-secure-store';
 
 export default function Profile() {
-    const [ proresults, setProResults ] = useState('');
+    const [ proresults, setProResults ] = useState(null);
     // const [ libresults, setLibResults ] = useState([]);
     
     useEffect(() => {
-        GetUserProfile('april2').then(r=>setProResults(r));
-        // GetUserLibrary('april').then(s=>setLibResults(s));
-    }, [])
+        async function checkData() {
+            const data = await GetUserProfile('april2'); //will need to replace with props
+            setProResults(data);
+        }
+        checkData();
 
+    }, [])
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Paige's Profile</Text>
-            {/* <View>
-                {proresults.map(user => <Text key={user.Index}> {user.UserName} </Text>)}
-            </View> */}
-
-            {/* <ScrollView>
-                {libresults.map(book => <Text style={globalStyles.item} key={book.Index}> {book.Name} </Text>)}
-            </ScrollView> */}
-            {/* <View style={globalStyles.profileInfo}>
-                <Text>About me info!</Text>
-                <Button
-                title='Profile info'
-                onPress={() => GetUserLibrary('Frederica Greenhill').then(r=>setResults(r)) } />
-            </View>
-            <ScrollView>
-                {results.map(book => <Text style={globalStyles.item} key={book.Index}> {book.Title} </Text>)}
-            </ScrollView> */}
+            {proresults ? (
+                <Text>{proresults}</Text> //still working to get these to display
+            ):(
+                <View>
+                    <ActivityIndicator size="large" color="#99ff99" />
+                </View>
+            )}
         </View>
     );
 }
