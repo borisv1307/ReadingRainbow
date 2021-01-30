d266c4286eff66a2b6d0a720cabf0eb014edb92f
mbb83 <mbb83@drexel.edu>
2021-01-29 21:52:38 -0500

    Enabled book details to be displayed after searching

--- ReadingRainbow/screens/friend_list.js
+++ ReadingRainbow/screens/friend_list.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import { ScrollView, FlatList, Button, TextInput, TouchableOpacity, View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { ScrollView, FlatList, Button, TextInput, TouchableOpacity, View, Text, ActivityIndicator } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { useNavigation } from '@react-navigation/native';
 
 export default function FriendList() {
     const { navigate } = useNavigation('');
+    const [ friendResults, setFriendResults ] = useState();
+
+    // useEffect(() => {
+    //     AsyncStorage.getItem('username').then(user => {
+    //         GetFriendList(user).then(friend => setFriendResults(friend));
+    //     });
+    // }, []);
+
     return (
         <View style={globalStyles.container}>
-          <Text style={globalStyles.titleText}>Friends List</Text>
-          <TextInput style={globalStyles.input}
-            keyboardType='default'
-            placeholder='Enter Username'
-            onChangeText={(val) => setName(val)} />
-          <Button title='Search!' />
-          <TouchableOpacity onPress={() => navigate('Profile')}>
-            <FlatList
-                data={[
-                    {key: 'Yang Wen-Li'},
-                    {key: 'Reinhard von Lohengramm'},
-                    {key: 'Oskar von Reuenthal'},
-                    {key: 'Julian Minci'},
-                ]}
-                renderItem={({item}) => <Text style={globalStyles.item}>{item.key}</Text>}
-                />
-            </TouchableOpacity>
+            <Text style={globalStyles.titleText}>Friends List</Text>
+            {(friendResults) ?
+                <View>
+                    {/* friend results displayed here */}
+                </View>
+            :
+                <ActivityIndicator color="black"/>
+            }
         </View>
         
     );
